00fac5a216f9b269edfad67ba7a48e8bedd217fd
Marcelo Carneiro <marcelo@countrhq.com>
2020-12-03 10:35:34 +0100

    feat: first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+/* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react'
+import { atom, Provider, useAtom } from 'jotai'
+import './App.css'
+
+const todoAtoms = atom((async) =>
+  fetch('https://jsonplaceholder.typicode.com/todos').then((res) => res.json())
+)
+const filterAtom = atom('')
+
+function FilterInput() {
+  const [filter, filterSet] = useAtom(filterAtom)
+
+  return (
+    <input className='customInput' value={filter} onChange={(evt) => filterSet(evt.target.value)} />
+  )
+}
+
+const filteredTodos = atom((get) =>
+  get(todoAtoms).filter((todo) => todo.title.toLowerCase().includes(get(filterAtom).toLowerCase()))
+)
+
+function TodoTable() {
+  const [todos] = useAtom(filteredTodos)
+
+  return (
+    <div>
+      <ul className='todoGride'>
+        {todos.map((todo) => (
+          <li key={todo.id}>{todo.title}</li>
+        ))}
+      </ul>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <FilterInput />
+      <TodoTable />
     </div>
-  );
+  )
 }
 
-export default App;
+const Bootstrap = () => (
+  <Provider>
+    <React.Suspense fallback={<div>loading...</div>}>
+      <App />
+    </React.Suspense>
+  </Provider>
+)
+
+export default Bootstrap
